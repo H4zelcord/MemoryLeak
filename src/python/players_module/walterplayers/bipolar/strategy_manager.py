831d534d027f3271bb8b045ffbe68217a140c865
patxisantos <patxisantosrivas@gmail.com>
2023-10-21 16:51:20 +0200

    Arreglo strategy manager

--- src/python/players_module/walterplayers/bipolar/strategy_manager.py
+++ src/python/players_module/walterplayers/bipolar/strategy_manager.py
@@ -3,9 +3,11 @@ from walterplayers.bipolar.constants import AdviserMode
 class StrategyManager:
     ''' Manager of strategy. Given defensive and ofensive limit, a strategy will be choosen. '''
 
-    def __init__(self, defensive_limit, offensive_limit):
-        self._previous_adviser_mode = AdviserMode.Offensive
-        self._adviser_mode = AdviserMode.Offensive
+    def __init__(self, ultra_offensive_limit, offensive_limit, defensive_limit, ultra_defensive_limit):
+        self._adviser_mode = AdviserMode.UltraOffensive
+        self._previous_adviser_mode = AdviserMode.UltraOffensive
+        self._ultra_offensive_limit = ultra_offensive_limit
+        self._offensive_limit = offensive_limit
         self._defensive_limit = defensive_limit
         self._ultra_defensive_limit = ultra_defensive_limit
         self._ultra_offensive_mode_limit = None
@@ -26,20 +28,28 @@ class StrategyManager:
             #First time that the life points is updated, limits should be compute
             self._ultra_offensive_mode_limit = self._ultra_offensive_limit * life_points
             self._offensive_mode_limit = self._offensive_limit * life_points
-            print('Mode limits initialized with Deffensive Mode Limit : ' +
-                str(self._defenvise_mode_limit) + ' and Offensive Mode Limit: ' +
-                str(self._offensive_mode_limit))
-
-        if (AdviserMode.Offensive == self._adviser_mode
-            and life_points <= self._defenvise_mode_limit):
-            self._previous_adviser_mode = self._adviser_mode
+            self._defensive_mode_limit = self._defensive_limit * life_points
+            self._ultra_defensive_mode_limit = self._ultra_defensive_limit * life_points
+           
+            print('Mode limits initialized with UltraOffensive Mode Limit : ' +
+                str(self._ultra_offensive_mode_limit) + ' Offensive Mode Limit: ' +
+                str(self._offensive_mode_limit) + ' Defensive Mode Limit: ' +
+                str(self._defensive_mode_limit) + ' and UltraDefensive Mode Limit: ' +
+                str(self._ultra_defensive_mode_limit)
+            )
+        
+        if life_points > 0 and life_points < self._ultra_defensive_mode_limit:
+            self._adviser_mode = AdviserMode.UltraDefensive
+            self._previous_adviser_mode = AdviserMode.Defensive
+        elif self._ultra_defensive_limit <= life_points and life_points < self._defensive_mode_limit:
             self._adviser_mode = AdviserMode.Defensive
-            self._previous_adviser_mode = AdviserMode.Offensive
+            self._previous_adviser_mode = AdviserMode.UltraDefensive
         elif self._defensive_limit <= life_points and life_points < self._offensive_mode_limit:
             self._adviser_mode = AdviserMode.Offensive
-            return
-
-        self._previous_adviser_mode = self._adviser_mode
+            self._previous_adviser_mode = AdviserMode.UltraOffensive
+        elif self._offensive_limit <= life_points and life_points < self._ultra_offensive_mode_limit:
+            self._adviser_mode = AdviserMode.UltraOffensive
+            self._previous_adviser_mode = AdviserMode.Offensive
 
     def is_strategy_changed(self):
         ''' Return True if life point changes strategy to follow up. '''
@@ -59,6 +69,9 @@ if __name__ == "__main__":
     print(strategyManager.get_adviser_mode())
     print(strategyManager.is_strategy_changed())
     strategyManager.update_life_points(150)
+    print(strategyManager.get_adviser_mode())
+    print(strategyManager.is_strategy_changed())
+    strategyManager.update_life_points(120)
     print(strategyManager.get_adviser_mode())
     print(strategyManager.is_strategy_changed())
     strategyManager.update_life_points(90)
