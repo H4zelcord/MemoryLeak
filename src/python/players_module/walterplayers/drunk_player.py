115d2159547ad82db19bdf208d35f3c32e92ac76
patxisantos <patxisantosrivas@gmail.com>
2023-10-21 12:29:04 +0200

    drunk_player solo MOVE

--- src/python/players_module/walterplayers/drunk_player.py
+++ src/python/players_module/walterplayers/drunk_player.py
@@ -7,16 +7,12 @@ class DrunkPlayer(BasePlayer):
     ''' Drunk player will choose one random available action  '''
 
     def choose_action(self, find_response):
+        
         available_actions = list(Action)
 
-        if not self.is_possible_attack(find_response):
-            available_actions.remove(Action.ATTACK)
-
-        if not self.is_possible_move(find_response):
-            available_actions.remove(Action.MOVE)
-
-        result_action = choice(available_actions)
+        result_action = Action.MOVE
         print(find_response)
+
         match result_action:
             case Action.ATTACK:
                 return result_action, choice(self.get_id_ias(find_response))
